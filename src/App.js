b47c3ee61c32ecb2f51fc695c23cf39068ebe436
Jorge <xadrijo@gmail.com>
2025-12-12 14:44:28 -0300

    Added sass and fixed bug in h2 tag in App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './categories.styles.scss'
+
 const App = () => {
 
   const categories = [
@@ -25,10 +27,11 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map((title) => (
-        <div className="category-container">
-          <div className="vategory-body-container">
-            <h2>{title}</h2>
+      {categories.map(({ title, id }) => (
+        <div key={id} className="category-container">
+          <div className="background-image"></div>
+          <div className="category-body-container">
+            <h2>{ title }</h2>
             <p>Shop Now</p>
           </div>
         </div>
