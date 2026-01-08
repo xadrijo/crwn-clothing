2030a46d29fe231366cff7504662bd40071ecadf
Jorge <xadrijo@gmail.com>
2026-01-08 20:32:45 -0300

    Added User Context

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
+
+import { UserContext } from "../../contexts/user.context";
 
 import { 
     signInWithGooglePopup, 
@@ -20,6 +22,8 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
 
+    const { setCurrentUser } = useContext(UserContext);
+
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     };
@@ -33,8 +37,12 @@ const SignInForm = () => {
         event.preventDefault();
 
         try {
-            const response = await singInAuthUserWithEmailAndPassword(email, password);
-            console.log(response);
+            const { user } = await singInAuthUserWithEmailAndPassword(
+                email, 
+                password
+            );
+            setCurrentUser(user);
+            
             resetFormFields();
         } catch(error) {
             switch(error.code) {
