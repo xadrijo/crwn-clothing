f09a152fe20fcd764b98a136962655caca469998
Jorge <xadrijo@gmail.com>
2026-01-09 13:21:07 -0300

    Finalizing Auth Listener

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,9 +1,7 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
-
-import { UserContext } from "../../contexts/user.context";
 
 import { 
     signInWithGooglePopup, 
@@ -22,28 +20,22 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
 
-    const { setCurrentUser } = useContext(UserContext);
-
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     };
 
     const signInWithGoogle = async () => {
-        const { user } = await signInWithGooglePopup();
-        await createUserDocumentFromAuth(user);
-
-        setCurrentUser(user);
+        await signInWithGooglePopup();
     };
 
     const handleSubmit = async(event) => {
         event.preventDefault();
 
         try {
-            const { user } = await singInAuthUserWithEmailAndPassword(
+            await singInAuthUserWithEmailAndPassword(
                 email, 
                 password
             );
-            setCurrentUser(user);
 
             resetFormFields();
         } catch(error) {
