f8bae8238c695d1b3d2edbc5330944c4f87aa2a8
Jorge <xadrijo@gmail.com>
2026-01-10 16:38:52 -0300

    Checkout Item part 2

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -32,12 +32,15 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     );
 }
 
+const clearCartItem = (cartItems, cartItemToClear) => cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
     removeItemFromCart: () => {},
+    clearItemFromCart: () => {},
     cartCount: 0
 });
 
@@ -59,11 +62,16 @@ export const CartProvider = ({ children }) => {
         setCartItems(removeCartItem(cartItems, cartItemToRemove));
     }
 
+    const clearItemFromCart = (cartItemToClear) => {
+        setCartItems(clearCartItem(cartItems, cartItemToClear));
+    }
+
     const value = {
         isCartOpen, 
         setIsCartOpen, 
         addItemToCart,
-        removeItemFromCart, 
+        removeItemFromCart,
+        clearItemFromCart, 
         cartItems, 
         cartCount
     };
